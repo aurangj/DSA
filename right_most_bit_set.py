a407c82c7e1c4c40b3becdcdc77ec51a837d61fa
aurangj <hi.jayesh@gmail.com>
2019-01-13 19:40:45 +0000

    assignments

---
+++ right_most_bit_set.py
@@ -0,0 +1,9 @@
+a  = 44
+b = 1
+print bin(a)
+
+if a & b == 1:
+    print 'Right most bit is set'
+else:
+    print ' right most bit is not set'
+
---
+++ string_anagrams.py
@@ -0,0 +1,44 @@
+
+# code to check if the strings are anagrams
+# assumption is made that all the letters are used only once.
+from operator import xor
+
+string1 = "Adultery"
+string2 = "true lad"
+res1 = 1
+for a in string1.lower():
+    if a != " ":
+        res1 = xor(res1, ord(a))
+
+res2 = 1
+for a in string2.lower():
+    if a != " ":
+        res2 = xor(res2, ord(a))
+
+if res1 == res2:
+    print "String is an anagram"
+else:
+    print "String is not anagram"
+
+
+
+
+
+# arr = []
+# arr2 = []
+# for c in string1:
+#     if c != " ":
+#        arr.append(ord(c))
+#
+# for c in string2:
+#     if c != " ":
+#         arr2.append(ord(c))
+#
+# res1 = 1
+# for a in arr:
+#     res1 = xor(res1, a)
+#
+# res2 = 1
+# for a in arr2:
+#     res2 = xor(res2, a)
+
