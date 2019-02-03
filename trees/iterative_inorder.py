d2e90c99f728ce80f02e864d8333df8522171b43
aurangj <hi.jayesh@gmail.com>
2019-02-03 15:19:48 +0000

    Add files via upload

--- trees/iterative_inorder.py
+++ trees/iterative_inorder.py
@@ -51,26 +51,20 @@ def iterative_preorder(root):
             stack.append(root)
 
 #[10, 19, 14, 31, 42, 35, 27]
+# code was copied from online... as multiple attempts did not work
 def iterative_postorder(root):
     stack = []
     visited = []
     stack.append(root)
-    if root.right is not None:
-        stack.append(root.right)
-    while len(stack) > 0:
-        while root.left is not None and root.left.data not in visited:
-
-            #stack.append(root.left)
-            root = root.left
-            if root.right is not None:
-                stack.append(root.right)
-            visited.append(root.data)
+    while len(stack) > 0 :
+        root = stack.pop()
+        visited.append(root.data)
+        if root.left is not None:
+            stack.append(root.left)
         if root.right is not None:
             stack.append(root.right)
-            visited.append(root.data)
+    return visited[::-1]
 
-        root = stack.pop()
-    return visited
 
 # Preorder traversal
 # Root -> Left ->Right
@@ -83,27 +77,26 @@ def PreorderTraversal( root):
 
     return res
 
-def PostrderTraversal( root):
+
+def PostorderTraversal(root):
     res = []
     if root:
-
-        res = res + PreorderTraversal(root.left)
-        res = res + PreorderTraversal(root.right)
+        res = PostorderTraversal(root.left)
+        res = res + PostorderTraversal(root.right)
         res.append(root.data)
     return res
 
 
 
 if __name__ == '__main__':
-    root = Node(27)
-    root.insert(14)
-    root.insert(35)
-    root.insert(60)
-    root.insert(19)
-    root.insert(91)
-    root.insert(42)
+    root = Node(1)
+    root.insert(3)
+    root.insert(4)
+    root.insert(6)
+
     print(iterative_postorder(root))
-    print(PostrderTraversal(root))
+    print(PostorderTraversal(root))
+
 
 
 
